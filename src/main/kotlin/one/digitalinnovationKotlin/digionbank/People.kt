bd66745f8104e03d780d01bfc329bd40575b3cd1
Radrick <radrickgomes2708@hotmail.com>
2021-02-26 00:01:26 -0300

    visibility of the cpf method set to private

--- src/main/kotlin/one/digitalinnovationKotlin/digionbank/People.kt
+++ src/main/kotlin/one/digitalinnovationKotlin/digionbank/People.kt
@@ -4,7 +4,15 @@ package one.digitalinnovationKotlin.digionbank
 class People {
 
     var name: String = "João"
+
+    //private set of cpf
     var cpf: String = "122.221.345-67"
+    private set
+
+    //internal class
+    /*inner class Address {
+        var street: String = "Rua dez"
+    }*/
 
 }
 
